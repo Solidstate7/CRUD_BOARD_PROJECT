4bea5ab04d9c588cd6390c6448538d87aef1cad2
Eunjae Lee <136287051+Solidstate7@users.noreply.github.com>
2023-09-27 21:35:15 +0900

    add/fix: account.repo CRUD methods 구현, template 2개 삭제, 디렉토리 수정

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express()
 const nunjucks = require('nunjucks')
 const { SERVER_PORT, NODE_ENV } = require('dotenv')
 const router = require('./src/index.route')
+const pool = require('./lib/poolPromise')
 const cookieParser = require('cookie-parser')
+const middleware = require('./src/auth/auth.middleware')
 
 app.set('view engine', 'html')
 nunjucks.configure('templates', {
@@ -13,12 +15,15 @@ nunjucks.configure('templates', {
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.use(cookieParser())
+app.use(middleware.auth)
 
 app.use(router)
 
 app.listen(SERVER_PORT || 3000, async () => {
     try {
-
+        const connection = await pool.getConnection()
+        console.log('DB Connected');
+        connection.release()
     } catch (e) {
         throw new Error("SERVER ERROR ", e.message)
     }
--- src/account/account.repo.js
+++ src/account/account.repo.js
@@ -6,11 +6,10 @@ exports.createAccount = async (user_id, user_pw, lvl = 1) => {
     const valueArr = [user_id, user_pw, lvl]
 
     const [{affectedRows}] = await pool.query(sql, valueArr)
-    console.log(affectedRows);
-    console.log('안');
+    
     if(!affectedRows) return false
-    console.log('될리없지');
     return true
+
     } catch (e) {
         throw new Error('Repo Error ', e.message)
     }
@@ -18,27 +17,48 @@ exports.createAccount = async (user_id, user_pw, lvl = 1) => {
 
 exports.searchAccount = async (user_id, user_pw) => {
     try {
-    const sql = ''
-    } catch (e) {
+    const sql = `SELECT * FROM accounts WHERE user_id=? AND user_pw=?`
+    const valueArr = [user_id, user_pw]
 
+    const [[result]] = await pool.query(sql, valueArr)
+
+    return result
+
+    } catch (e) {
+        throw new Error('Repo Error ', e.message)
     }
 
 }
 
-exports.modifyAccount = async (user_pw) => {
+exports.updateAccount = async (new_pw, user_pw) => {
     try {
+    const sql = `UPDATE accounts SET user_pw=? WHERE user_pw=?`
+    const valueArr = [new_pw, user_pw]
+
+    const [{changedRows}] = await pool.query(sql, valueArr)
+    
+    if (!changedRows) return false
+    return true
 
     } catch (e) {
-
+        throw new Error('Repo Error ', e.message)
     }
 }
 
 exports.deleteAccount = async (user_pw) => {
     try {
+    const sql = `DELETE FROM accounts WHERE user_pw=?`
+    const valueArr = [user_pw]
+    
+    const [{affectedRows}] = await pool.query(sql, valueArr)
+
+    if (!affectedRows) return false
+    return true
 
     } catch (e) {
-
+        throw new Error('Repo Error ', e.message)
     }
 }
 
-exports.createAccount('eunjae4', '1234');
+exports.createAccount('eunjae', '1234');
+exports.deleteAccount('1234');
