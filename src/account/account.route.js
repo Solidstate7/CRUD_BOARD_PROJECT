dd72452756de6375572e735bdee97ef7bb1c779d
Eunjae Lee <136287051+Solidstate7@users.noreply.github.com>
2023-10-04 16:18:54 +0900

    add: account 가입, 로그인, 로그아웃

--- src/account/account.route.js
+++ src/account/account.route.js
@@ -3,9 +3,17 @@ const router = express.Router();
 const accountController = require("./account.controller");
 
 router.get("/signin", accountController.getSignin);
-router.get("/signout", accountController.getSignout);
 router.post("/signin", accountController.postSignin);
+
 router.get("/signup", accountController.getSignup);
+router.post("/signup", accountController.postSignup);
+
+router.get("/signout", accountController.getSignout);
+
 router.get("/mypage", accountController.getMypage);
+router.get("/modify", accountController.getEdit);
+router.post("/modify", accountController.postEdit);
+
+router.post("/delete", accountController.postDelete);
 
 module.exports = router;
